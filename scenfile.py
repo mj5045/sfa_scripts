3c147800f2a08e16ebfb9d65792cf34d63eee60d
mj5045 <mitchelljanek5045@gmail.com>
2020-09-21 15:50:26 -0500

    SceneFile#04

--- scenfile.py
+++ scenfile.py
@@ -2,12 +2,13 @@ from pathlib import Path
 
 class SceneFile(object):
 
-    def __init__(self, folder_path, descriptor, task, ver, ext):
-        self.folder_path = Path(folder_path)
-        self.descriptor = descriptor
-        self.task = task
-        self.ver = ver
-        self.ext = ext
+    def __init__(self, path):
+        self.folder_path = Path()
+        self.descriptor = 'main'
+        self.task = None
+        self.ver = 1
+        self.ext = '.ma'
+        self._init_from_path(path)
 
     @property
     def filename(self):
@@ -19,6 +20,14 @@ class SceneFile(object):
     @property
     def path(self):
         return self.folder_path / self.filename
+    
+    def _init_from_path(self, path):
+        path = Path(path)
+        self.folder_path = path.parent
+        self.ext = path.suffix
+        self.descriptor, self.task, ver = path.stem.split("_")
+        self.ver = int(ver.split("v")[-1])
 
-scene_file = SceneFile("D:\\", "tank", "model", 1, ".ma")
+scene_file = SceneFile("D:/tank_model_v001.ma")
 print(scene_file.path)
+print(scene_file.filename)
