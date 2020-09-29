5246c1621149f84d1419434d8f3a0489662b0c5f
mj5045 <mitchelljanek5045@gmail.com>
2020-09-29 17:05:38 -0500

    SceneFile#06

--- src/scenefile.py
+++ src/scenefile.py
@@ -1,6 +1,9 @@
-import pymel.core as pmc
-from pymel.core.system import Path 
+import logging
 
+import pymel.core as pmc
+from pymel.core.system import Path
+
+log = logging.getLogger(__name__)
 
 class SceneFile(object):
 
@@ -16,19 +19,26 @@ class SceneFile(object):
     def filename(self):
         pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
         return pattern.format(descriptor=self.descriptor,
-                          task=self.task,
-                          ver=self.ver,
-                          ext=self.ext)
+                              task=self.task,
+                              ver=self.ver,
+                              ext=self.ext)
+
     @property
     def path(self):
         return self.folder_path / self.filename
-    
+
     def _init_from_path(self, path):
         path = Path(path)
-        self.folder_path = pat
-        self.descriptor, self.task, ver = path.name.struptext().split("_")
+        self.folder_path = path.parent
+        self.ext = path.ext
+        self.descriptor, self.task, ver = path.name.stripext().split("_")
         self.ver = int(ver.split("v")[-1])
 
     def save(self):
+        try:
+            return pmc.system.saveAs(self.path)
+        except RuntimeError as err:
+            log.warning("Missing directories in path.  Creating folders...")
+            self.folder_path.makedirs_p()
+            return pmc.system.saveAs(self.path)
 
-        return pmc.system.saveAs(self.path)
