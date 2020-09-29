f45c3a17d4f9958dfc42250edc72e510616521c8
mj5045 <mitchelljanek5045@gmail.com>
2020-09-29 16:02:18 -0500

    clear

---
+++ src/ScriptEditor.py
@@ -0,0 +1,7 @@
+import scenefile
+reload(scenefile)
+
+scene_file = SceneFile("C:\\Scripts\\sandbox\\tank_model_v001.ma")
+scene_file.filename
+scene_file.path
+scene_file.save()
---
+++ src/scenfile.py
@@ -0,0 +1,35 @@
+import pymel.core as pmc
+from pymel.core.system import Path 
+
+
+class SceneFile(object):
+
+    def __init__(self, path):
+        self.folder_path = Path()
+        self.descriptor = 'main'
+        self.task = None
+        self.ver = 1
+        self.ext = '.ma'
+        self._init_from_path(path)
+
+    @property
+    def filename(self):
+        pattern = "{descriptor}_{task}_v{ver:03d}{ext}"
+        return pattern.format(descriptor=self.descriptor,
+                          task=self.task,
+                          ver=self.ver,
+                          ext=self.ext)
+    @property
+    def path(self):
+        return self.folder_path / self.filename
+    
+    def _init_from_path(self, path):
+        path = Path(path)
+        self.folder_path = path.parent
+        self.ext = path.ext
+        self.descriptor, self.task, ver = path.name.stripext().split("_")
+        self.ver = int(ver.split("v")[-1])
+
+    def save(self):
+
+        return pmc.system.saveAs(self.path)
