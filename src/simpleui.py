1270d3979290506929bb51a324a7798718ed33da
mj5045 <mitchelljanek5045@gmail.com>
2020-10-12 14:17:05 -0500

    simpleut#02

--- src/simpleui.py
+++ src/simpleui.py
@@ -1,9 +1,16 @@
-import QtWidgets as QtWidgets
+import maya.OpenMayaUI as omui
+from PySide2 import QtWidgets
+from shiboken2 import wrapInstance
 
+
+def maya_main_window():
+
+    main_windows = omui.MQtUtil.mainWindow()
+    return wrapInstance(Long(main_windows), QtWidgets.QWidget)
 
 class SimpleUI(QtWidgets.QDialog):
 
     def __init__(self):
         """super is parent for simpleui which is qdialog"""
-        super(SimpleUI, self).__init__()
+        super(SimpleUI, self).__init__(parent=maya_main_window())
         self.setWindowTitle("A Simple Ui")
